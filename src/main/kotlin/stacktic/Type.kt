1db776566f0fb532313273b689204d2694462ac7
Nathan Schulzke <nschulzke@gmail.com>
2022-07-29 10:54:39 -0600

    Make Type a Value

--- src/main/kotlin/stacktic/Type.kt
+++ src/main/kotlin/stacktic/Type.kt
@@ -1,13 +1,21 @@
 package stacktic
 
-sealed interface Type {
+sealed interface Type : Value {
+    object TypeLiteral : Type {
+        override val type: Type = TypeLiteral
+        override fun toString() = "Type"
+    }
+
     object String : Type {
+        override val type: Type = TypeLiteral
         override fun toString() = "String"
     }
     object Integer : Type {
+        override val type: Type = TypeLiteral
         override fun toString() = "Integer"
     }
     object Double : Type {
+        override val type: Type = TypeLiteral
         override fun toString() = "Double"
     }
 
